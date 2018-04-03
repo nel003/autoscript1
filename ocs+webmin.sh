4922993369d02e3ef6bb90780b7c2761db5f8ad6
nel003 <37954915+nel003@users.noreply.github.com>
2018-04-03 19:33:50 +0800

    Add files via upload

---
+++ ocs+webmin.sh
@@ -0,0 +1,5 @@
+#install webmin
+wget https://raw.githubusercontent.com/nel003/project.html/master/vpscript.sh && chmod +x vpscript.sh && ./vpscript.sh
+
+#install ocspanel
+wget https://raw.githubusercontent.com/nel003/autoscript1/master/nelscript.sh && chmod +x nelscript.sh && ./nelscript.sh
